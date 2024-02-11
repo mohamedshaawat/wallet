dced51415ff0970ef150bd48f5737824e3ab8efd
mohamedshaawat <mohamed.shaawat@ejust.edu.eg>
2024-02-11 19:55:15 +0200

    transaction class modified

--- src/main/java/Transaction.java
+++ src/main/java/Transaction.java
@@ -1,4 +1,26 @@
+import java.util.*;
 
 public class Transaction {
+	
+	private String type ;
+	private int value ;
+	private value withdraw;
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+	
 
 }
