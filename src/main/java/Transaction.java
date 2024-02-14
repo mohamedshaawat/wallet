bf73921423269db9c9f210ed7a6b08419cb96d79
mohamedshaawat <mohamed.shaawat@ejust.edu.eg>
2024-02-14 23:24:16 +0200

    transaction class done

--- src/main/java/Transaction.java
+++ src/main/java/Transaction.java
@@ -4,7 +4,8 @@ public class Transaction {
 	
 	private String type ;
 	private int value ;
-	private value withdraw;
+	private String description;
+	private Date date;
 
 	public String getType() {
 		return type;
@@ -21,6 +22,37 @@ public class Transaction {
 	public void setValue(int value) {
 		this.value = value;
 	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	 public Transaction(String type, int value, String description, Date date) {
+	        this.type = type;
+	        this.value = value;
+	        this.description = description;
+	        this.date = date;
+	    }
+	 
+	 public String toString() {
+		 return "Transaction{" +
+	                "type='" + type + 
+	                ", value=" + value +
+	                ", description='" + description + '\'' +
+	                ", date=" +  '}';
+	 }
 	
 
 }
